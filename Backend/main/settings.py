4c5c57f1d699e55485697fbf6b1ae947ef6f8346
tiomashimon <tiomashimon@gmai.com>
2023-11-23 21:58:10 +0100

    Secret issue

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,6 +91,7 @@ import environ
 import os
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env("SECRET_KEY")
 
 
 DATABASES = {
