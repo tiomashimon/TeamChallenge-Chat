eb3ba7037fe492db2c5cb77f69176bce59449c81
tiomashimon <tiomashimon@gmai.com>
2023-11-23 21:12:08 +0100

    env example

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -17,10 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8$u9n1jfuj&b$94hlmqs4262$6(tzqc#af2+pemt2#--mj6=@j"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +86,6 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 import environ
 import os
