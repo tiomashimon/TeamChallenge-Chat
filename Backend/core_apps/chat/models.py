7f3f7cd77f9591dca5f9421725d617535129ff2d
Dmytro Petrykiv <petrykiv.dmytro19@gmail.com>
2023-11-25 23:55:36 +0200

    Fixed deletion_time in Chat model and user on_delete.

--- Backend/core_apps/chat/models.py
+++ Backend/core_apps/chat/models.py
@@ -4,16 +4,21 @@ from core_apps.user.models import User
 
 
 class Chat(models.Model):
+    DELETE_TIME_CHOICES = [
+        (72, "72 hours"),
+        (48, "48 hours"),
+    ]
+
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     is_alive = models.BooleanField(default=True)
-    deletion_time = models.DateTimeField(null=True, blank=True)
+    deletion_time = models.CharField(max_length=2, choices=DELETE_TIME_CHOICES, default=72)
     users = models.ManyToManyField(User, related_name="chats")
 
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     content = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
