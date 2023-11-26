45d31016ddeb55939565ed5c6d23956a4af2ef97
i3oi3ka <i3oi3ka@ukr.net>
2023-11-26 20:34:47 +0200

    feat

--- Backend/core_apps/user/views.py
+++ Backend/core_apps/user/views.py
@@ -8,32 +8,27 @@ import random
 import string
 
 
-
 class RegistrationView(SuccessMessageMixin, CreateView):
     template_name = 'user/register.html'
     form_class = UserRegistrationForm
     success_url = reverse_lazy('register')
     success_message = "Your profile was created successfully"
 
-
     def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            context['form'] = UserRegistrationForm()  
-            return context
-
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserRegistrationForm()
+        return context
 
     def generate_random(self):
         password_length = 8
         characters = string.ascii_letters + string.digits
         return ''.join(random.choice(characters) for i in range(password_length))
-    
-    def form_valid(self, form):
 
+    def form_valid(self, form):
         form.instance.username = self.generate_random()
         form.instance.set_password(self.generate_random())
 
         user = form.save()
-
         login(self.request, user)
 
-        return super().form_valid(form) 
+        return super().form_valid(form)
