ef3cb2db0fb68f6b5b5a8d903a0993c98ad3ed8b
tiomashimon <tiomashimon@gmai.com>
2023-11-26 17:01:20 +0100

    test

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production1
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
