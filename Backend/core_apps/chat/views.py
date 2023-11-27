a57875f82d7deab102db6a0c07643ab1c5c5c421
tiomashimon <tiomashimon@gmai.com>
2023-11-27 20:40:49 +0100

    last issue

--- Backend/core_apps/chat/views.py
+++ Backend/core_apps/chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 
-def (request):
+def HelloWorld(request):
     greetings = 'Hello World!'
     return JsonResponse({'message': greetings})
 
--- Backend/core_apps/user/views.py
+++ Backend/core_apps/user/views.py
@@ -3,21 +3,19 @@ from .forms import UserRegistrationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
-import random
-import string
-
 from django.http import JsonResponse
-from django.utils.datastructures import MultiValueDictKeyError
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 import string
 import random
 
 @method_decorator(csrf_exempt, name='dispatch')
 class RegistrationView(View):
+    template_name = 'user/register.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('register')
 
     def generate_random(self):
         password_length = 8
@@ -33,7 +31,7 @@ class RegistrationView(View):
         username = self.generate_random()
         password = self.generate_random()
         form_data = {'nickname': nickname, 'username': username, 'password1': password, 'password2': password}
-        form = UserCreationForm(form_data)
+        form = self.form_class(form_data)  # Use your custom form here
 
         if form.is_valid():
             user = form.save()
@@ -43,33 +41,5 @@ class RegistrationView(View):
             return JsonResponse({'success': False, 'error': 'Invalid registration credentials'})
 
     def get(self, request, *args, **kwargs):
-        return JsonResponse({'success': 'Yes'})
-
-
-
-
-
-class BackendRegistrationView(SuccessMessageMixin, CreateView):
-    template_name = 'user/register.html'
-    form_class = UserRegistrationForm
-    success_url = reverse_lazy('register')
-    success_message = "Your profile was created successfully"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = UserRegistrationForm()
-        return context
-
-    def generate_random(self):
-        password_length = 8
-        characters = string.ascii_letters + string.digits
-        return ''.join(random.choice(characters) for i in range(password_length))
-
-    def form_valid(self, form):
-        form.instance.username = self.generate_random()
-        form.instance.set_password(self.generate_random())
-
-        user = form.save()
-        login(self.request, user)
-
-        return super().form_valid(form)
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
