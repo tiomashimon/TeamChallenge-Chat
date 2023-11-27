f1505a4372f3ad0adefb676ee71a128381299106
tiomashimon <tiomashimon@gmai.com>
2023-11-27 19:56:33 +0100

    Registration View

--- Backend/core_apps/user/views.py
+++ Backend/core_apps/user/views.py
@@ -6,9 +6,41 @@ from django.urls import reverse_lazy
 import random
 import string
 
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.utils.datastructures import MultiValueDictKeyError
+
+@method_decorator(csrf_exempt, name='dispatch')
+class RegistrationView(View):
+
+    def generate_random(self):
+        password_length = 8
+        characters = string.ascii_letters + string.digits
+        return ''.join(random.choice(characters) for i in range(password_length))
+
+    def post(self, request, *args, **kwargs):
+        try:
+            nickname = request.POST['nickname']
+        except MultiValueDictKeyError:
+            return JsonResponse({'success': False, 'error': 'Invalid form data'})
+        username = self.generate_random()
+        password = self.generate_random()
+        form = UserRegistrationForm({'nickname': nickname,'username':usename, 'password1': password, 'password2': password})
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'error': 'Invalid registration credentials'})
 
 
-class RegistrationView(SuccessMessageMixin, CreateView):
+
+
+class BackendRegistrationView(SuccessMessageMixin, CreateView):
     template_name = 'user/register.html'
     form_class = UserRegistrationForm
     success_url = reverse_lazy('register')
@@ -30,4 +62,5 @@ class RegistrationView(SuccessMessageMixin, CreateView):
 
         user = form.save()
         login(self.request, user)
+
         return super().form_valid(form)
