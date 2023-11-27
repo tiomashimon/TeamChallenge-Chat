d1c6225c84a5c17788f759fcac3b028493a22479
tiomashimon <tiomashimon@gmai.com>
2023-11-27 20:26:03 +0100

    Solve

--- Backend/core_apps/user/views.py
+++ Backend/core_apps/user/views.py
@@ -6,12 +6,15 @@ from django.urls import reverse_lazy
 import random
 import string
 
-from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views import View
-from django.utils.decorators import method_decorator
 from django.utils.datastructures import MultiValueDictKeyError
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+import string
+import random
 
 @method_decorator(csrf_exempt, name='dispatch')
 class RegistrationView(View):
@@ -19,23 +22,29 @@ class RegistrationView(View):
     def generate_random(self):
         password_length = 8
         characters = string.ascii_letters + string.digits
-        return ''.join(random.choice(characters) for i in range(password_length))
+        return ''.join(random.choice(characters) for _ in range(password_length))
 
     def post(self, request, *args, **kwargs):
         try:
             nickname = request.POST['nickname']
         except MultiValueDictKeyError:
             return JsonResponse({'success': False, 'error': 'Invalid form data'})
+
         username = self.generate_random()
         password = self.generate_random()
-        form = UserRegistrationForm({'nickname': nickname,'username':usename, 'password1': password, 'password2': password})
-        
+        form_data = {'nickname': nickname, 'username': username, 'password1': password, 'password2': password}
+        form = UserCreationForm(form_data)
+
         if form.is_valid():
             user = form.save()
             login(request, user)
             return JsonResponse({'success': True})
         else:
             return JsonResponse({'success': False, 'error': 'Invalid registration credentials'})
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({'success': 'Yes'})
+
 
 
 
