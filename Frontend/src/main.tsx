7eef61ca53ebc675cafe14abe8847d6dd10df0ef
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-11-29 18:56:54 +0200

    feat: temporarily disabled redux

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 import { setupStore } from './store/store.ts'
 
-const store = setupStore();
+// const store = setupStore();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
 )
