d2b5fd96c28e04a18b17bd526da070f45d603015
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-11-29 20:58:13 +0200

    fix: added commentary

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // import { Provider } from 'react-redux'
-import { setupStore } from './store/store.ts'
+// import { setupStore } from './store/store.ts'
 
 // const store = setupStore();
 
