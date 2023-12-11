1a8ed0e00cf2297093add9627a51bd348005fcc1
tiomashimon <tiomashimon@gmai.com>
2023-12-11 18:49:19 +0100

    Add Topic, Filtering ang Pagination

--- Backend/core_apps/chat/admin.py
+++ Backend/core_apps/chat/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Chat, Message
+from .models import Chat, Message, ChatTopic
 
 
 admin.site.register(Chat)
 admin.site.register(Message)
+admin.site.register(ChatTopic)
--- Backend/core_apps/chat/urls.py
+++ Backend/core_apps/chat/urls.py
@@ -9,11 +9,20 @@ message_list = views.MessageViewSet.as_view(
     }
 )
 
+topic_list = views.TopicViewSet.as_view(
+    {
+        'get': 'list',
+        'post': 'create'
+    }
+)
+
 urlpatterns = [
     path("create/", views.ChatCreate.as_view(), name='chats'),
-    path('<int:id>/detail/', views.ChatRetrieve.as_view(), name='chat-detail'),
+    path('<int:id>/detail/', views.ChatRetrieve.as_view(), name='chat'),
     path('', views.ChatList.as_view(), name='add-user'),
-    path('<int:pk>/delete/', views.ChatDestroy.as_view(), name='chat-delete'),
+    path('<int:pk>/delete/', views.ChatDestroy.as_view(), name='chat'),
 
-    path('<int:id>/message/', message_list, name='message-list'), 
+    path('<int:id>/message/', message_list, name='message'), 
+    path('topics/', topic_list, name='topic'), 
 ]
+
