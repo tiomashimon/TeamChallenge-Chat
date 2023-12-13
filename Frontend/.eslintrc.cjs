573c381dc52119f1ccac95df327b014921928d68
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-12-13 15:09:46 +0200

    feat: added mini configs

--- Frontend/.eslintrc.cjs
+++ Frontend/.eslintrc.cjs
@@ -5,9 +5,10 @@ module.exports = {
 		'airbnb',
 		'airbnb-typescript',
 		'airbnb/hooks',
+    'plugin:react/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
-    "plugin:prettier/recommended"
+    'plugin:prettier/recommended'
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
@@ -22,6 +23,8 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': 'off',
 	},
 	parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
 		project: ['./Frontend/tsconfig.json', './Frontend/tsconfig.node.json'],
 	},
 };
