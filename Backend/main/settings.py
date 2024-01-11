3f2af827f6bd4a38250a6f6ecb84aec30f0ae411
tiomashimon <tiomashimon@gmai.com>
2024-01-11 11:22:13 +0100

    Media URL

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -186,6 +186,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
