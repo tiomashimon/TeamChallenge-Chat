17b800a9da4b751f6a7150f80a15787d56cc0eec
tiomashimon <tiomashimon@gmai.com>
2024-01-30 18:29:30 +0100

    Celery queue and fixed second Worker

--- Backend/main/celery.py
+++ Backend/main/celery.py
@@ -10,7 +10,10 @@ app = Celery('main')
 
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
+app.conf.task_routes = {
+    'core_apps.chat.tasks.delete_expired_chats':{'queue':'queue1'},
+    'core_apps.user.tasks.send_mail_to_user':{'queue':'queue2'},
+                        }
 app.autodiscover_tasks()
 
 
